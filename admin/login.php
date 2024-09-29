349930a09f062cb5d3198a6160caf5b2b2ae78f6
AlomLu <abdurrahmanalom@gmail.com>
2024-09-29 18:01:05 +0600

    Admin Login, Logout, Showin Cagtegory in Admin Panel

--- admin/login.php
+++ admin/login.php
@@ -1,3 +1,16 @@
+<?php 
+	include '../lib/Session.php'; 
+	Session::init();
+?>
+
+<?php include '../config/config.php'; ?>
+<?php include '../lib/Database.php'; ?>
+<?php include '../helpers/Format.php'; ?>
+
+<?php 
+	$db = new Database();
+	$fm = new Format();
+?>
 <!DOCTYPE html>
 <head>
 <meta charset="utf-8">
@@ -7,13 +20,45 @@
 <body>
 <div class="container">
 	<section id="content">
-		<form action="" method="post">
+		<?php
+			if($_SERVER['REQUEST_METHOD'] == 'POST'){
+				$username = $fm->validation($_POST['username']);
+				$password = $fm->validation(md5($_POST['password']));
+
+				$username = mysqli_real_escape_string($db->link, $username);
+				$password = mysqli_real_escape_string($db->link, $password);
+
+				if($username == "" || $password == ""){
+					$error = "Field must not be empty";
+				}else{
+					$query = "SELECT * FROM tbl_user WHERE username = '$username' AND password = '$password' ";
+					$result = $db->select($query);
+
+					if($result != false){
+						$value = mysqli_fetch_array($result);
+						$row = mysqli_num_rows($result);
+						if($row > 0){
+							Session::set("login", true);
+							Session::set("username", $value['username']);
+							Session::set("userID", $value['id']);
+
+							header("Location: index.php"); // admin er index
+						}else{
+							echo "<span style='color: red; font-size: 18px;'>No Result found</span>";
+						}
+					}else{
+						echo "<span style='color: red; font-size: 18px;'>Username or Password not matched!!..</span>";
+					}
+				}
+			}
+		?>
+		<form action="login.php" method="post">
 			<h1>Admin Login</h1>
 			<div>
-				<input type="text" placeholder="Username" required="" name="username"/>
+				<input type="text" required="" name="username" placeholder="Username"/>
 			</div>
 			<div>
-				<input type="password" placeholder="Password" required="" name="password"/>
+				<input type="password" required="" name="password" placeholder="Password"/>
 			</div>
 			<div>
 				<input type="submit" value="Log in" />
